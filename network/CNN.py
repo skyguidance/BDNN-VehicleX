d641055e64b27fa6fe9ee0e02c2885f134417759
skyguidance <46770626+skyguidance@users.noreply.github.com>
2021-05-26 15:50:04 +0800

    Fix Shape Mismatch for CNN.

--- network/CNN.py
+++ network/CNN.py
@@ -12,9 +12,14 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.backbone, self.backbone_output_dim = get_backbone(config)
         self.fc = nn.Linear(self.backbone_output_dim, config["model"]["output_dims"])
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
 
     def forward(self, x):
         x = self.forward_backbone(x)
+        # Needs GAP.
+        if len(x.shape) == 4 and x.shape[3] != 1:
+            x = self.gap(x)
+        x = x.flatten(1)
         x = self.fc(x)
         return x
 
